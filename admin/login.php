1207150122b5b219bfd730ac36950437cb689bca
FidyahNSalsabila <fidyahslsbl92@gmail.com>
2023-12-22 19:10:18 +0800

    init

---
+++ admin/login.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="login.css">
+</head>
+<body>
+    <div style="overflow: hidden;" class="container" >
+        <img class="satu" src="/pictures/login1.svg" alt="">
+        <h3>C<span style="color: var(--primary);">r</span>a<span style="color: var(--primary);">f</span>topia.<span style="color: var(--primary);">id</span></h3>
+        <form action="">
+            <label for="username">Username</label>
+            <input type="text" id="username" required>
+            <label for="password">Kata Sandi</label>
+            <input type="text" id="password" required>
+            <button>Masuk</button>
+        </form>
+        <img class="dua" src="/pictures/login2.svg" alt="">
+    </div>
+</body>
+</html>
---
+++ detail.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Details</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="./css/detail.css">
+</head>
+<body>
+    <nav>
+        <div class="logo">
+            <h1>C<span style="color: var(--primary);" >r</span>a<span style="color: var(--primary);" >f</span>topia.id</h1>
+        </div>
+        <ul>
+            <li><a href="./index.php">Beranda</a></li>
+            <li><a href="./katalog.php">Katalog</a></li>
+            <li><a href="./kontak.php">Kontak Kami</a></li>
+        </ul>
+        <div class="contact">
+            <i class="bi bi-tiktok"></i>
+            <i class="bi bi-twitter"></i>
+            <i class="bi bi-instagram"></i>
+        </div>
+    </nav>
+    <div>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/XopP25MVFwo?si=veFYkJs9lWUe6YwK" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    </div>
+    <div class="container">
+        <div class="gambar">
+            <img src="/pictures/section1.svg" alt="">
+            <img src="/pictures/section1.svg" alt="">
+            <img src="/pictures/section1.svg" alt="">
+        </div>
+        <div class="judul">
+            <h3>JUDUL KERAJINAN</h3>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corporis adipisci quo praesentium, harum temporibus sapiente perspiciatis totam delectus consequuntur impedit? Necessitatibus officia nisi laboriosam officiis ad architecto placeat neque voluptatum?</p>
+            <h3>LANGKAH-LANGKAH</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum optio fugiat deleniti, recusandae dignissimos voluptates praesentium nostrum neque quaerat provident autem quae? Similique id fugit architecto commodi fugiat, unde beatae!</p>
+        </div>
+    </div>
+</body>
+</html>
